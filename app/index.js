f19a73f9bc1c553bc185aebbcde4dce8bcda9f75
Blake Coffee <blakecoffee@Blakes-MacBook-Pro-3.local>
2023-11-21 16:17:30 -0600

    Floating search bar

--- app/index.js
+++ app/index.js
@@ -1,13 +1,35 @@
 // Home.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import Footer from '../components/Footer';
 import Map from '../components/Map';
+import HomeSearch from '../components/HomeSearch';
+import { useNavigation } from 'expo-router';
 
 const Home = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const removeHeader = () => {
+      navigation.setOptions({
+        headerShown: false,
+      });
+    };
+
+    removeHeader();
+
+    return () => {
+      // Reset header options if the component is unmounted
+      navigation.setOptions({
+        headerShown: true,
+      });
+    };
+  }, [navigation]);
+
   return (
     <SafeAreaView style={styles.container}>
       <Map />
+      <HomeSearch />
       <Footer />
     </SafeAreaView>
   );
