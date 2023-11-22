67137ea8699fdd9d734edbe9a6a9039b78a47850
Blake Coffee <blakecoffee@Blakes-MacBook-Pro-3.local>
2023-11-22 08:07:21 -0600

    Getting some styling done on the dealbox

--- components/DealDisplay.js
+++ components/DealDisplay.js
@@ -1,9 +1,6 @@
-// DealDisplay.js
 import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-
 import { ScrollView } from 'react-native';
-
 
 const DealDisplay = ({ setSelectedBusinessLocation }) => {
   const [businesses, setBusinesses] = useState([]);
@@ -13,14 +10,14 @@ const DealDisplay = ({ setSelectedBusinessLocation }) => {
     const fetchBusinesses = async () => {
       try {
         const userLocation = await getUserLocation();
-        
+
         if (userLocation && userLocation.latitude !== 0 && userLocation.longitude !== 0) {
           const formattedCoordinates = {
             latitude: userLocation.latitude,
             longitude: userLocation.longitude,
             radius: 1000,
           };
-  
+
           const response = await fetch('http://192.168.1.29:4444/business', {
             method: 'POST',
             headers: {
@@ -28,20 +25,20 @@ const DealDisplay = ({ setSelectedBusinessLocation }) => {
             },
             body: JSON.stringify(formattedCoordinates),
           });
-  
+
           if (!response.ok) {
             throw new Error(`Failed to fetch businesses. Server response: ${response.statusText}`);
           }
-  
+
           const data = await response.json();
-  
+
           const businessesWithDistance = data.data.map((business) => ({
             ...business,
             distance: calculateDistance(userLocation, business.location.coordinates),
           }));
-  
+
           const sortedBusinesses = businessesWithDistance.sort((a, b) => a.distance - b.distance);
-  
+
           setBusinesses(sortedBusinesses);
         } else {
           console.log('Please share your location in order to view businesses near you');
@@ -97,8 +94,8 @@ const DealDisplay = ({ setSelectedBusinessLocation }) => {
   };
 
   return (
-    <ScrollView contentContainerStyle={styles.scrollContainer}>
-      <View style={styles.dealDisplayContainer}>
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.dealDisplayContainer}>
         <Text style={styles.dealHeader}>Deals Near You</Text>
         {businesses.map((business) => (
           <View key={business.business_name}>
@@ -123,41 +120,49 @@ const DealDisplay = ({ setSelectedBusinessLocation }) => {
             )}
           </View>
         ))}
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  dealDisplayContainer: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
+  container: {
+    ...StyleSheet.absoluteFillObject, // Takes the full screen space
     bottom: 0,
+    justifyContent: 'flex-end', // Align content at the bottom
   },
-  scrollContainer: {
-    flexGrow: 1,
-    width: '80%',
-    borderRadius: 30,
+  dealDisplayContainer: {
+    width: '95%', // Take up 100% of the screen width
+    backgroundColor: 'white', // Set background color
+    borderTopLeftRadius: 20, // Add rounded corners to the top-left and top-right
+    borderTopRightRadius: 20,
+    padding: 20, // Add padding to the content
+    height: '50%',
+    top: '50%',
+    left: '2.5%'
   },
   dealHeader: {
-    // Add styles for the header
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
   dealButtonDisplay: {
-    // Add styles for the deal button display
+    marginBottom: 15,
   },
   dealTitle: {
-    // Add styles for the deal title
+    fontSize: 16,
+    fontWeight: 'bold',
   },
   dealBusinessName: {
-    // Add styles for the business name
+    color: 'gray',
+    marginBottom: 5,
   },
   dealDescription: {
-    // Add styles for the deal description
+    marginBottom: 10,
   },
   dealDistance: {
-    // Add styles for the deal distance
+    color: 'blue',
+    fontStyle: 'italic',
   },
 });
 
