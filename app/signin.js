54a5ef13d1a08b39976dc88b79bcf4fa3b5a0b4b
Blake Coffee <blake.coffee8@gmail.com>
2023-11-28 23:13:31 -0600

    Styling the signin page

--- app/signin.js
+++ app/signin.js
@@ -1,7 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Alert, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Alert,
+  StyleSheet,
+  KeyboardAvoidingView,
+  ScrollView,
+  Animated,
+  Keyboard,
+} from 'react-native';
 import { useAuth } from './authcontext'; // Update the path
 import { useNavigation } from 'expo-router';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 const SignIn = () => {
   const { signIn } = useAuth();
@@ -9,19 +23,62 @@ const SignIn = () => {
     Email: '',
     Password: '',
   });
-
   const navigation = useNavigation();
+  const imageScale = useRef(new Animated.Value(1)).current;
+  const signInContainerBottom = useRef(new Animated.Value(-200)).current;
+
+  let [fontsLoaded] = useFonts({
+    'Poppins-Regular': require('../assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+    'Poppins-Black': require('../assets/fonts/Poppins-Black.ttf'),
+    'Poppins-Medium': require('../assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-SemiBold': require('../assets/fonts/Poppins-SemiBold.ttf')
+  });
 
 
   useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
-      navigation.setOptions({
-        headerShown: false,
-      });
-    });
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      () => {
+        Animated.parallel([
+          Animated.timing(imageScale, {
+            toValue: 0.5,
+            duration: 250,
+            useNativeDriver: false,
+          }),
+          Animated.timing(signInContainerBottom, {
+            toValue: 10,
+            duration: 250,
+            useNativeDriver: false,
+          }),
+        ]).start();
+      }
+    );
 
-    return unsubscribe;
-  }, [navigation]);
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => {
+        Animated.parallel([
+          Animated.timing(imageScale, {
+            toValue: 0.5,
+            duration: 0,
+            useNativeDriver: false,
+          }),
+          Animated.timing(signInContainerBottom, {
+            toValue: 10, // Adjust the value to your preference
+            duration: 0,
+            useNativeDriver: false,
+          }),
+        ]).start();
+      }
+    );
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, [imageScale, signInContainerBottom]);
+
 
   const handleSubmit = async () => {
     console.log('Sending sign-in request:', formData);
@@ -67,34 +124,51 @@ const SignIn = () => {
     }
   };
 
+  if (!fontsLoaded){
+    return <AppLoading/>
+  }
   return (
-    <View style={styles.container}>
-      <View style={styles.signInContainer}>
-        <Text style={styles.title}>Login</Text>
-        <Text style={styles.subtitle}>Please sign in to continue</Text>
-        <TextInput
-          style={styles.customInput}
-          placeholder="Email"
-          value={formData.Email}
-          onChangeText={(text) => setFormData({ ...formData, Email: text })}
-          keyboardType="email-address"
-          autoCapitalize="none"
-          autoCorrect={false}
-          required
+    <KeyboardAvoidingView
+      behavior="height"
+      style={{ flex: 1, backgroundColor: '#ffffff' }}
+    >
+      <ScrollView>
+        <Animated.Image
+          source={require('../assets/images/Whir-Logo-V2-Square-Stacked.png')}
+          style={[styles.image, { transform: [{ scale: imageScale }] }]}
         />
-        <TextInput
-          style={styles.customInput}
-          placeholder="Password"
-          value={formData.Password}
-          onChangeText={(text) => setFormData({ ...formData, Password: text })}
-          secureTextEntry
-          required
-        />
-        <TouchableOpacity onPress={handleSubmit} style={styles.customButton}>
-          <Text style={styles.buttonText}>Sign In</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+        <Animated.View
+          style={[
+            styles.signInContainer,
+            { bottom: signInContainerBottom },
+          ]}
+        >
+          <Text style={styles.title}>Login</Text>
+          <Text style={styles.subtitle}>Please sign in to continue</Text>
+          <TextInput
+            style={styles.customInput}
+            placeholder="Email"
+            value={formData.Email}
+            onChangeText={(text) => setFormData({ ...formData, Email: text })}
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoCorrect={false}
+            required
+          />
+          <TextInput
+            style={styles.customInput}
+            placeholder="Password"
+            value={formData.Password}
+            onChangeText={(text) => setFormData({ ...formData, Password: text })}
+            secureTextEntry
+            required
+          />
+          <TouchableOpacity onPress={handleSubmit} style={styles.customButton}>
+            <Text style={styles.buttonText}>Sign In</Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -107,15 +181,29 @@ const styles = StyleSheet.create({
     },
     signInContainer: {
       width: '80%',
+      bottom: '10%',
+      alignSelf: 'center'
+    },
+    image: {
+      width: 300,
+      height: 300,
+      top: '10%',
+      alignSelf: 'center'
     },
     title: {
       fontSize: 24,
       fontWeight: 'bold',
       marginBottom: 10,
+      fontFamily: 'Poppins-Black',
+      color: '#fca502',
+      fontSize: 50,
+      marginBottom: 1
     },
     subtitle: {
       fontSize: 16,
       marginBottom: 20,
+      fontFamily: 'Poppins-Regular',
+      marginBottom: 5
     },
     customInput: {
       height: 40,
@@ -123,16 +211,25 @@ const styles = StyleSheet.create({
       borderWidth: 1,
       marginBottom: 10,
       paddingHorizontal: 10,
+      fontFamily: 'Poppins-SemiBold',
+      
     },
     customButton: {
-      backgroundColor: '#007bff', // Set your desired button color
+      backgroundColor: '#fca502', // Set your desired button color
       padding: 10,
-      borderRadius: 5,
+      borderRadius: 30,
       alignItems: 'center',
+      width: 100,
+      paddingVertical: 15,
+      alignSelf: 'flex-end',
+      marginTop: 5,
+      
     },
     buttonText: {
       color: '#ffffff', // Set your desired text color
       fontWeight: 'bold',
+      fontFamily: 'Poppins-Bold'
+
     },
   });
 
