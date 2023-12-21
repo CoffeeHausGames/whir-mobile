52493ed10d21faa87f8b44a8d8f2fa3d932d55e8
lucas-kern <lucasdkern@gmail.com>
2023-12-20 23:45:01 -0600

    remove google sign in

--- app/signin.js
+++ app/signin.js
@@ -17,7 +17,6 @@ import { useAuth } from './authcontext'; // Update the path
 import { useNavigation } from 'expo-router';
 import { useFonts } from 'expo-font';
 import { apiRequest } from '../app/networkController';
-import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
 
 const SignIn = () => {
   const { signIn } = useAuth();
@@ -175,30 +174,6 @@ const SignIn = () => {
           <TouchableOpacity onPress={handleSubmit} style={styles.customButton}>
             <Text style={styles.buttonText}>Sign In</Text>
           </TouchableOpacity>
-          <Button
-            title={'Sign in with Google'}
-            onPress={() => {
-              GoogleSignin.configure({
-                androidClientId: 'ADD_YOUR_ANDROID_CLIENT_ID_HERE',
-                iosClientId: 'ADD_YOUR_iOS_CLIENT_ID_HERE',
-              });
-              GoogleSignin.hasPlayServices()
-                .then((hasPlayService) => {
-                  if (hasPlayService) {
-                    GoogleSignin.signIn()
-                      .then((userInfo) => {
-                        console.log(JSON.stringify(userInfo));
-                      })
-                      .catch((e) => {
-                        console.log('ERROR IS: ' + JSON.stringify(e));
-                      });
-                  }
-                })
-                .catch((e) => {
-                  console.log('ERROR IS: ' + JSON.stringify(e));
-                });
-            }}
-          />
           <View style={styles.registerView}>
             <Text style={styles.signUpNoButtonText}>
               Don't have an account?
